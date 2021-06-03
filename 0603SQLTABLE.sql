7db9510c429470a773662b80db102c4738b252a6
kjh <kangwjdgus928@gmail.com>
2021-06-03 16:46:02 +0900

    완료

--- 0603SQLTABLE.sql
+++ 0603SQLTABLE.sql
@@ -2,7 +2,7 @@ create table C_info(
     mbti char(4) constraint C_info_mbti_nn not null,
     id number(2) primary key,
     name varchar2(10) constraint C_info_name_nn not null,
-    img varchar2(100) constraint C_info_img_nn not null,
+    img varchar2(200) constraint C_info_img_nn not null,
     birth date constraint C_info_birth_nn not null,
     debut date constraint C_info_debut_nn not null
 );
@@ -20,5 +20,12 @@ create table relation(
     bad number(2) constraint  ralation_bad_fk  references C_info(id)
 );
 
-insert into
+insert into CAREER values(01, '2020/12/20','baeksangaward','runningman')
+insert into CAREER values(02, '2020/10/20','Popular Culture and Arts Award','knowingbro')
+insert into CAREER values(03, '2017/12/30', 'SBS Entertainment Awards', 'What do you do when you play?')
+insert into CAREER values(04, '2020/12/19', 'SBS Entertainment Awards', 'Alley Restaurant')
 
+insert into RALATION values(01, 02, 03)
+insert into RALATION values(02, 01, 04)
+insert into RALATION values(03, 04, 01)
+insert into RALATION values(04, 03, 02)
